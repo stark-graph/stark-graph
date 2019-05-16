5494a8e2c875d9549beb380ef65d3681ff34d401
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-05-16 08:32:44 -0400

    linux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import { hot } from 'react-hot-loader/root';
 import React from 'react';
 
 const App = props => {
@@ -6,4 +7,4 @@ const App = props => {
   return <div>hi hi</div>;
 };
 
-export default App;
+export default hot(App);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
-var status = process.env.NODE_ENV; //taken from script so we don't have to flip mode when using development/production
+var status = process.env.NODE_ENV || 'development'; //taken from script so we don't have to flip mode when using development/production
 var path = require('path');
+const ExtractCSS = new (require('extract-text-webpack-plugin'))('style.css');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.jsx',
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, '/build'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
   },
 
@@ -13,18 +17,31 @@ module.exports = {
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: ['.ts', '.tsx', '.js', '.json', '.jsx'],
+    extensions: ['.js', '.jsx'],
   },
   mode: status,
 
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build'),
+    hot: true,
+  },
   module: {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by babel-loader
-      { test: /.tsx?$/, exclude: /node-modules/, use: 'babel-loader' },
-      { test: /.jsx?$/, exclude: /node-modules/, use: 'babel-loader' },
       { test: /.js?$/, exclude: /node-modules/, use: 'babel-loader' },
+      { test: /\.css$/, use: ExtractCSS.extract('css-loader') },
+      // { test: /.tsx?$/, exclude: /node-modules/, use: 'babel-loader' },
+      // { test: /.jsx?$/, exclude: /node-modules/, use: 'babel-loader' },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       // { enforce: 'pre', test: /.js$/, exclude: /node-modules/, loader: 'source-map-loader' },
     ],
   },
+  plugins: [
+    ExtractCSS,
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Hot Module Replacement',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 };
