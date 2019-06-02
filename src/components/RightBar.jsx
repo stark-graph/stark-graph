b793067223a838a0cf04d04fe8ba2b7cf2b20f2b
Nel Malikova <gunelmalikova@gmail.com>
2019-06-02 17:05:26 -0400

    make sidebar display correct code

--- src/components/RightBar.jsx
+++ src/components/RightBar.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import AlgoSelector from './AlgoSelector.jsx';
 
-const RightBar = () => (
-  <div
-    className="right-bar"
-    style={{
-      width: '40%',
-      height: 'auto',
-    }}
-  >
-    <AlgoSelector />
-  </div>
-);
+const RightBar = props => {
+  const { algoName, activeTree } = props;
+  return (
+    <div
+      className="right-bar"
+      style={{
+        width: '40%',
+        height: 'auto',
+      }}
+    >
+      <AlgoSelector algoName={algoName} activeTree={activeTree} />
+    </div>
+  );
+};
 
 export default RightBar;
