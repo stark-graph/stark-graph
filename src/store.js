ee8c9d4c781554b2153919a4f178c01895a6d0a0
Nel Malikova <gunelmalikova@gmail.com>
2019-06-18 19:06:50 -0400

    clean up

--- src/store.js
+++ src/store.js
@@ -1,6 +1,5 @@
 import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './reducers';
 
-const store = createStore(reducers, composeWithDevTools());
+const store = createStore(reducers);
 export default store;
