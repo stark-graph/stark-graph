1c5b66f9adffb83c06cc66345f76a13e82ac5c1e
Nel Malikova <gunelmalikova@gmail.com>
2019-06-20 18:11:54 -0400

    edit server file

--- server/server.js
+++ server/server.js
@@ -8,9 +8,9 @@ const port = process.env.PORT || 3000;
 app.use('/build', express.static(path.join(__dirname, '../build')));
 app.use('/', express.static(path.join(__dirname, '../public')));
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../public/index.html'));
-// });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/'));
