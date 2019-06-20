89b3050956201abbc533294b0bda87edea602679
Nel Malikova <gunelmalikova@gmail.com>
2019-06-20 18:14:12 -0400

    edits

--- server/server.js
+++ server/server.js
@@ -5,8 +5,9 @@ const path = require('path');
 
 const port = process.env.PORT || 3000;
 
-app.use('/build', express.static(path.join(__dirname, '../build')));
+// app.use('/build', express.static(path.join(__dirname, '../build')));
 app.use('/', express.static(path.join(__dirname, '../public')));
+app.use('/', express.static(path.join(__dirname, '../build')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
